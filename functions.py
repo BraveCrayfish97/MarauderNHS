6fe4e4e33fcd65052ce9e150b5d01b0a6caa589d
BraveCrayfish97 <64327453+BraveCrayfish97@users.noreply.github.com>
2023-06-06 19:22:24 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,21 @@ import boto3
 import uuid
 from datetime import datetime
 from boto3.dynamodb.conditions import Key, Attr
+import os
+from numpy import void
+import asyncio
+# https://frankie567.github.io/httpx-oauth/oauth2/
+from httpx_oauth.clients.google import GoogleOAuth2
+from dotenv import load_dotenv
+import requests
+
+#GOOGLE OAUTH CONFIG-------------
+load_dotenv('.env')
+
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+REDIRECT_URI = os.environ['REDIRECT_URI']
+#---------------------------------
 
 access_key = "AKIAQXLOFPE7NQYAORED"
 secret_key = "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"
@@ -65,8 +80,8 @@ def make_link(url):
 
 def show_student_hours(response):
     
-    cols   = sl.columns([0.5,3,2,1,1,1,2])
-    fields = ["#","email", "month", "day", "hours", "mins","Button"]
+    cols   = sl.columns([0.5,3,2,1,1.5,1.5,2,2,2])
+    fields = ["#","email", "month", "day", "hours", "mins", "location", "desc","Image"]
 
     # header
     for col, field in zip(cols, fields):
@@ -74,17 +89,73 @@ def show_student_hours(response):
     # rows
     for idx, row in enumerate(response):
         #sl.write(row)
-        cols = sl.columns([0.5,3,2,1,1,1,2])
+        cols = sl.columns([0.5,3,2,1,1.5,1.5,2,2,2])
         cols[0].write(idx)
         for i in range(1,len(fields)-1):
             
             cols[i].write(row[fields[i]])
         placeholder = cols[len(fields)-1]
-        show_more   = placeholder.button("more", key=idx, type="primary")
+        show_more   = placeholder.button("View Image", key=idx, type="primary")
         if show_more:
             # rename button
             placeholder.button("Close", key=str(idx)+"_")
             # show image
             display_s3_file(row["filename"])
-            
-            
+def check_if_new_student(email):
+    response = dynamodb.Table("NHS_Students").query(
+        KeyConditionExpression=Key('email').eq(email)
+    )
+    return response['Items']==[]
+#----------------------------
+#GOOGLE OAUTH FUNCS
+async def get_authorization_url(client: GoogleOAuth2, redirect_uri: str):
+    authorization_url = await client.get_authorization_url(redirect_uri, scope=["profile", "email"])
+    return authorization_url
+
+
+async def get_access_token(client: GoogleOAuth2, redirect_uri: str, code: str):
+    token = await client.get_access_token(code, redirect_uri)
+    return token
+
+
+async def get_email(client: GoogleOAuth2, token: str):
+    user_id, user_email = await client.get_id_email(token)
+    return user_id, user_email
+
+
+def get_login_str():
+    client: GoogleOAuth2 = GoogleOAuth2(CLIENT_ID, CLIENT_SECRET)
+    authorization_url = asyncio.run(
+        get_authorization_url(client, REDIRECT_URI))
+    return f'''<a target = "_self" href ="{authorization_url}"> Sign In </a> '''
+
+
+def auth_user() -> void:
+    client: GoogleOAuth2 = GoogleOAuth2(CLIENT_ID, CLIENT_SECRET)
+    # get the code from the url
+    code = sl.experimental_get_query_params()['code']
+    token = asyncio.run(get_access_token(
+        client, REDIRECT_URI, code))
+    #sl.write(token)
+    user_id, user_email = asyncio.run(
+        get_email(client, token['access_token']))
+    sl.write(
+        f"You're logged in as {user_email}")
+    
+    url = f'https://people.googleapis.com/v1/{user_id}'
+    params = {
+        'personFields': 'names'  # Specify the desired profile fields
+    }
+    headers = {
+        'Authorization': f'Bearer {token["access_token"]}'
+    }
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        profile_data = response.json()
+        #sl.write(profile_data)
+        name = profile_data['names'][0]['displayNameLastFirst']
+        print(f'Name: {name}')
+        return user_email, name
+    else:
+        print(f'Error retrieving user profile: {response.status_code}\nPlease try again.')
