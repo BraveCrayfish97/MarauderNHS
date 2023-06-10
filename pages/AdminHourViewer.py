943c0d257d123e99f833fd96a5d448ba67183088
BraveCrayfish97 <64327453+BraveCrayfish97@users.noreply.github.com>
2023-06-10 14:09:09 -0500

    Update AdminHourViewer.py

--- pages/AdminHourViewer.py
+++ pages/AdminHourViewer.py
@@ -11,7 +11,8 @@ if "std_viewed" not in sl.session_state:
     sl.session_state["std_viewed"] = std_viewed
 
 if std_viewed | sl.session_state["std_viewed"]:
-    sl.session_state["std_viewed"]=True
-    data = query_for_email(std_email)
-    show_student_hours(data['Items'])
+    if std_email != "":
+        sl.session_state["std_viewed"]=True
+        data = query_for_email(std_email)
+        show_student_hours(data['Items'])
 
