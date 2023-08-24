5a34cec26fae5bdcf18622696c2da73faeec6e98
BraveCrayfish97 <64327453+BraveCrayfish97@users.noreply.github.com>
2023-08-23 22:31:58 -0500

    Update Meetings.py

--- pages/Meetings.py
+++ pages/Meetings.py
@@ -1,26 +1,38 @@
 import streamlit as sl
 from functions import *
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9 = sl.tabs(["Sep", "Oct", "Nov","Dec","Jan","Feb","Mar","Apr","May"])
+if "logged_in" in sl.session_state:
+    if sl.session_state["email"] in sl.session_state["admins"]:
 
-with tab1:
-   sl.header("September Meeting Attendance")
-   show_meeting_sheet("September")
-   sl.write(sl.session_state)
-with tab2:
-   sl.header("October Meeting Attendance")
-with tab3:
-   sl.header("November Meeting Attendance")
 
-with tab4:
-   sl.header("December Meeting Attendance")
-with tab5:
-   sl.header("January Meeting Attendance")
-with tab6:
-   sl.header("February Meeting Attendance")
+      tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9 = sl.tabs(["Sep", "Oct", "Nov","Dec","Jan","Feb","Mar","Apr","May"])
+      with tab1:
+         sl.header("September Meeting Attendance")
+         show_meeting_sheet("September")
+         submit_meeting_attendance("September")
 
-with tab7:
-   sl.header("March Meeting Attendance")
-with tab8:
-   sl.header("April Meeting Attendance")
-with tab9:
-   sl.header("May Meeting Attendance")
+      with tab2:
+         sl.header("October Meeting Attendance")
+      with tab3:
+         sl.header("November Meeting Attendance")
+
+      with tab4:
+         sl.header("December Meeting Attendance")
+      with tab5:
+         sl.header("January Meeting Attendance")
+      with tab6:
+         sl.header("February Meeting Attendance")
+
+      with tab7:
+         sl.header("March Meeting Attendance")
+      with tab8:
+         sl.header("April Meeting Attendance")
+      with tab9:
+         sl.header("May Meeting Attendance")
+
+
+
+    else:
+        sl.write("You do not have permission to view this page.")
+else:
+    sl.write("Please Login First")
+
