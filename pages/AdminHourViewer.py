b0b33016ef9e237dc2e6c74e237f8f0b4f5d21db
BraveCrayfish97 <64327453+BraveCrayfish97@users.noreply.github.com>
2023-08-23 22:31:14 -0500

    Update AdminHourViewer.py

--- pages/AdminHourViewer.py
+++ pages/AdminHourViewer.py
@@ -2,17 +2,23 @@ import streamlit as sl
 from functions import *
 
 
-sl.title("View Any Student's Hours Here")
 
-std_email = sl.text_input("Enter Student's Email")
-std_viewed = sl.button("Display Student's Hours")
 
-if "std_viewed" not in sl.session_state:
-    sl.session_state["std_viewed"] = std_viewed
+if "logged_in" in sl.session_state:
+    if sl.session_state["email"] in sl.session_state["admins"]:
+        sl.title("View Any Student's Hours Here")
+        std_email = sl.text_input("Enter Student's Email")
+        std_viewed = sl.button("Display Student's Hours")
 
-if std_viewed | sl.session_state["std_viewed"]:
-    if std_email != "":
-        sl.session_state["std_viewed"]=True
-        data = query_for_email(std_email)
-        show_student_hours(data['Items'])
+        if "std_viewed" not in sl.session_state:
+            sl.session_state["std_viewed"] = std_viewed
 
+        if std_viewed | sl.session_state["std_viewed"]:
+            if std_email != "":
+                sl.session_state["std_viewed"]=True
+                data = query_for_email(std_email)
+                show_student_hours(data['Items'])
+    else:
+        sl.write("You do not have permission to view this page.")
+else:
+    sl.write("Please Login First")
