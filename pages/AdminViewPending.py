c9e8489f9919e92debed346694aa064584c8dfbd
BraveCrayfish97 <64327453+BraveCrayfish97@users.noreply.github.com>
2023-08-23 22:31:37 -0500

    Update AdminViewPending.py

--- pages/AdminViewPending.py
+++ pages/AdminViewPending.py
@@ -1,6 +1,12 @@
 import streamlit as sl
 from functions import *
 
-data = scan_all_pending()
-show_student_pending_hours(data['Items'])#this will show all students's pending hours cuz we are giving data that has all students
 
+if "logged_in" in sl.session_state:
+    if sl.session_state["email"] in sl.session_state["admins"]:
+        data = scan_all_pending()
+        show_student_pending_hours(data['Items'])#this will show all students's pending hours cuz we are giving data that has all students
+    else:
+        sl.write("You do not have permission to view this page.")
+else:
+    sl.write("Please Login First")
