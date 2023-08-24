a87bd9ba42dc00671e8c5c37c791fc44e0736d54
BraveCrayfish97 <64327453+BraveCrayfish97@users.noreply.github.com>
2023-08-23 22:31:27 -0500

    Update AdminStudentViewer.py

--- pages/AdminStudentViewer.py
+++ pages/AdminStudentViewer.py
@@ -1,5 +1,12 @@
 import streamlit as sl
 from functions import *
 
-data = scan_all_students()
-show_all_students(data['Items'])
+
+if "logged_in" in sl.session_state:
+    if sl.session_state["email"] in sl.session_state["admins"]:
+        data = scan_all_students()
+        show_all_students(data['Items'])
+    else:
+        sl.write("You do not have permission to view this page.")
+else:
+    sl.write("Please Login First")
